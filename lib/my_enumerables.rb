8ba26779e9c88e83df4c06b00f97063b18407308
Nsikak Ekott <nsikakekott2006@gmail.com>
2024-01-17 21:54:49 +0100

    Custom Enumerable Project

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,71 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    for elem in self
+      yield(elem)
+    end
+  end
+
+  def my_map
+    result = []
+    for elem in self
+      result << yield(elem)
+    end
+    result
+  end
+
+  def my_each_with_index
+    result = []
+    self.length.times do |i|
+      result << self[i]
+      yield(self[i], i)
+    end
+    result
+  end
+
+  def my_all?
+    for elem in self
+      return false unless yield(elem)
+    end
+    true
+  end
+
+  def my_select
+    result = []
+    for elem in self
+      result << elem if yield(elem)
+    end
+    result
+  end
+
+  def my_count
+    count = 0
+    for elem in self
+      count += 1 if ( block_given? ? yield(elem) : true)
+    end
+    count
+  end
+
+  def my_any?
+    for elem in self
+      return true if yield(elem)
+    end
+    false
+  end
+
+  def my_none?
+    for elem in self
+      return false if yield(elem)
+    end
+    true
+  end
+
+  def my_inject (initial_value)
+    result = initial_value
+    for elem in self
+      result = yield(result, elem)
+    end
+    result
+  end
 end
